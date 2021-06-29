11a05642f45258e5fb68106434ee09c6a04b9797
heocoi <dev.hibiki@gmail.com>
2021-06-29 12:37:42 +0900

    [WIP] create button component story

---
+++ components/BHeader/BHeader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BHeader = props => {
+  return (
+    <div>
+Header
+    </div>
+  )
+}
+
+BHeader.propTypes = {
+
+}
+
+export default BHeader
--- theme.js
+++ theme.js
@@ -3,6 +3,16 @@ import { createMuiTheme } from '@material-ui/core/styles';
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
+    red: {
+      light: '#da4c5c',
+      main: '#ba2636',
+      dark: '#8b1c28',
+    },
+    yellow: {
+      light: '#ecc659',
+      main: '#e6b422',
+      dark: '#b18914',
+    },
     background: {
       default: '#fff',
     },
