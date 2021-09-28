420528d77f80c5a624c566c39bfebe171dec39bd
heocoi <dev.hibiki@gmail.com>
2021-09-28 19:41:22 +0900

    update link of child pages in mypage

--- pages/mypage.js
+++ pages/mypage.js
@@ -53,12 +53,15 @@ const notifications = [];
 const buttonLinks = [
   {id: 1, label: '基本情報', url: '/basic-information'},
   {id: 2, label: 'お届け先情報', url: '/delivery-info'},
+
+  // TODO: not implement yet
   {id: 4, label: '割引クーポン', url: '/'},
-  {id: 5, label: 'お問い合わせ一覧', url: '/contact'},
-  {id: 6, label: 'フォロー中の出品者一覧', url: '/'},
-  {id: 7, label: '返品/交換申請', url: '/'},
-  {id: 8, label: '返品/交換一覧', url: '/'},
-  {id: 9, label: 'パスワードを変更', url: '/'},
+
+  // TODO: not implement yet
+  // {id: 5, label: 'お問い合わせ一覧', url: '/contact'},
+  {id: 6, label: 'フォロー中の出品者一覧', url: '/followed-seller-list'},
+  {id: 7, label: '返品/交換申請', url: '/contact'},
+  {id: 9, label: 'パスワードを変更', url: '/auth/change-password'},
   {id: 3, label: '決済方法', url: '/payment-method'},
   {id: 10, label: 'ログアウト', url: '/'},
 ];
