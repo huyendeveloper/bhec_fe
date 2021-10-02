4cb65eafee18e77d42bff98bdd73fa3050344b38
heocoi <dev.hibiki@gmail.com>
2021-10-02 13:21:28 +0900

    update sentry config - new sntry project - add environment tag

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,9 +7,15 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://3a39d7591fee4648b3556654fb60e4dd@o868951.ingest.sentry.io/5824075',
+  dsn: SENTRY_DSN || 'https://974b67562a7842a0849370a0b757460d@o879587.ingest.sentry.io/5989150',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+
+  environment: process.env.APP_ENV || 'unknown',
+
+  // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-  tracesSampleRate: 1.0,
 });
