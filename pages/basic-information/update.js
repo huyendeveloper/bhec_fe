fdd07c059d429f8fc597776d92662438a052fbb3
trung <trungld1@icheck.vn>
2021-10-13 10:54:08 +0700

    fix bug update mail exits

--- pages/basic-information/update.js
+++ pages/basic-information/update.js
@@ -185,7 +185,7 @@ export default function BasicInformationUpdate() {
       setLoading(false);
       setAlerts({
         type: 'error',
-        message: res,
+        message: 'このメールアドレスはすでに登録されています',
       });
     }
   };
