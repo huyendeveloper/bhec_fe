2af779ee455ee2bcb8125b80bc0151bc71ab6428
anhtq-ict <anhtq.ict@gmail.com>
2021-10-18 16:04:47 +0700

    [Add] express delivery on product detail

--- components/SingleProduct/Meta.js
+++ components/SingleProduct/Meta.js
@@ -1,33 +1,59 @@
-import {Box, Table, TableBody, TableCell, TableContainer, TableRow} from '@material-ui/core';
+import {Box, makeStyles, Table, TableBody, TableCell, TableContainer, TableRow} from '@material-ui/core';
 import React from 'react';
 import {useRecoilValue} from 'recoil';
 
 import {productState} from '~/store/productState';
 
+const useStyles = makeStyles(() => ({
+  expressDeliveryCell: {
+    paddingTop: '0.5rem !important',
+  },
+}));
+
 const Meta = () => {
+  const classes = useStyles();
+
   const product = useRecoilValue(productState);
+
+  const expressDelivery = product?.sellerInfo?.express_delivery;
   const shippingType = product?.productDetail?.shipping_type || '';
   const shippingDate = product?.productDetail?.shipping_date || '';
   const shippingDays = product?.productDetail?.shipping_days || '';
 
   return (
-    <Box
-      component='div'
-    >
+    <Box component='div'>
       <TableContainer>
         <Table className={'table'}>
           <TableBody>
             {shippingType.length > 0 && (
-              <TableRow>
-                <TableCell
-                  component='th'
-                  scope='row'
-                >
-                  {'配送'}
-                </TableCell>
-                <TableCell align='left'>{shippingType}</TableCell>
-              </TableRow>
+              <>
+                <TableRow>
+                  <TableCell
+                    component='th'
+                    scope='row'
+                  >
+                    {'配送'}
+                  </TableCell>
+                  <TableCell align='left'>{shippingType}</TableCell>
+                </TableRow>
+                {expressDelivery && (
+                  <TableRow>
+                    <TableCell
+                      className={classes.expressDeliveryCell}
+                      component='th'
+                      scope='row'
+                    />
+                    <TableCell
+                      className={classes.expressDeliveryCell}
+                      align='left'
+                    >
+                      {`${expressDelivery}で配送する予定`}
+                    </TableCell>
+                  </TableRow>
+                )}
+              </>
             )}
+
             {shippingDays.length > 0 && shippingDays !== 'null' && (
               <TableRow>
                 <TableCell
