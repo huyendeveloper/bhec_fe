9c898d77999eb37ecfd133b034c9342b3ff3156e
anhtq-ict <anhtq.ict@gmail.com>
2021-10-28 15:06:24 +0700

    [Add] pagination for coupons page

--- pages/coupons/index.js
+++ pages/coupons/index.js
@@ -1,13 +1,15 @@
 import {Box, Typography, useMediaQuery, useTheme} from '@material-ui/core';
+import Pagination from '@material-ui/lab/Pagination';
 import {makeStyles} from '@material-ui/core/styles';
 import {signOut} from 'next-auth/client';
 import Image from 'next/image';
 import {useRouter} from 'next/router';
 import {useEffect, useState} from 'react';
 import {useRecoilState, useRecoilValue} from 'recoil';
+import PropTypes from 'prop-types';
 import Swal from 'sweetalert2';
 
-import {AlertMessageForSection, Button, ContentBlock} from '~/components';
+import {AlertMessageForSection, ContentBlock} from '~/components';
 import {ApplyCouponBar, CouponItem} from '~/components/Coupon';
 import {DefaultLayout} from '~/components/Layouts';
 import {getErrorMessage} from '~/lib/getErrorMessage';
@@ -20,11 +22,40 @@ const useStyles = makeStyles((theme) => ({
   couponContainer: {
     marginTop: '1rem',
   },
-  loadMoreButton: {
+  pagination: {
     display: 'flex',
-    alignItems: 'center',
     justifyContent: 'center',
-    marginTop: '1rem',
+    margin: '3rem 0',
+    [theme.breakpoints.down('md')]: {
+      margin: '1.5rem 0',
+    },
+    '& .MuiButtonBase-root': {
+      width: '3rem',
+      height: '3rem',
+      borderRadius: '50%',
+      border: '1px solid ' + theme.palette.grey.dark,
+      margin: '0 0.5rem',
+      background: theme.palette.white.main,
+      fontWeight: '700',
+      fontSize: '1rem',
+      color: theme.palette.gray.dark,
+      '&:hover': {
+        background: theme.palette.red.main,
+        borderColor: theme.palette.red.main,
+        color: theme.palette.white.main,
+      },
+      [theme.breakpoints.down('md')]: {
+        width: '2.5rem',
+        height: '2.5rem',
+        margin: '0 0.25rem',
+        fontSize: '0.875rem',
+      },
+    },
+    '& .Mui-selected': {
+      background: theme.palette.red.main,
+      borderColor: theme.palette.red.main,
+      color: theme.palette.white.main,
+    },
   },
   textCouponNull: {
     fontSize: '1.5rem',
@@ -41,7 +72,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const Coupons = () => {
+// Default
+const PER_PAGE = 2;
+
+const Coupons = ({queryParams}) => {
   const router = useRouter();
   const classes = useStyles();
   const theme = useTheme();
@@ -51,23 +85,21 @@ const Coupons = () => {
   const cartStateData = useRecoilValue(cartState);
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [canLoadMore, setCanLoadMore] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [countPages, setCountPages] = useState(0);
   const [alerts, setAlerts] = useState(null);
-  const [page, setPage] = useState(1);
   const [coupons, setCoupons] = useState([]);
   const [order, setOrder] = useRecoilState(orderState);
-
-  // Default
-  const PER_PAGE = 10;
 
   useEffect(() => {
     if (user?.isAuthenticated) {
       setIsAuthenticated(user?.isAuthenticated);
+      setCurrentPage(queryParams?.page ?? 0);
       fetchCoupons();
     } else {
       requestLogin();
     }
-  }, []);
+  }, [queryParams]);
 
   const requestLogin = () => {
     setUser({});
@@ -75,13 +107,15 @@ const Coupons = () => {
     router.push({pathname: '/auth/login'});
   };
 
-  const fetchCoupons = async (targetPage = 1) => {
-    const pageQuery = targetPage !== 1 && targetPage === page ? targetPage + 1 : targetPage;
+  const fetchCoupons = async () => {
+    const payload = {
+      page: queryParams?.page ?? 1,
+      per_page: queryParams?.per_page ?? 10,
+      order_by: 'id',
+      order: 'desc',
+    };
 
-    const {haveNextPage, userCoupons, error} = await CouponService.getCoupons({
-      page: pageQuery,
-      per_page: PER_PAGE,
-    });
+    const {page, pages, userCoupons, error} = await CouponService.getCoupons(payload);
 
     if (error) {
       if (error.errorCode) {
@@ -91,17 +125,20 @@ const Coupons = () => {
         });
       }
     } else {
-      setCoupons([...coupons, ...userCoupons]);
+      setCoupons(userCoupons);
     }
 
-    setCanLoadMore(haveNextPage);
+    setCurrentPage(page ?? 0);
+    setCountPages(pages ?? 0);
 
     return userCoupons;
   };
 
-  const loadMoreCoupons = () => {
-    fetchCoupons(page + 1);
-    setPage(page + 1);
+  const changePageCoupons = (_, pageNumber) => {
+    router.push({
+      pathname: '/coupons',
+      query: {...router.query, page: pageNumber},
+    });
   };
 
   const addCouponCode = async (couponCode) => {
@@ -117,10 +154,13 @@ const Coupons = () => {
         });
       }
     } else {
-      setCoupons([...coupons, userCoupon]);
       setAlerts({
         type: 'success',
         message: '入力されたクーポンは登録できました。',
+      });
+      router.push({
+        pathname: '/coupons',
+        query: {...router.query, page: 1},
       });
     }
 
@@ -177,16 +217,18 @@ const Coupons = () => {
             <>
               <div className={classes.couponContainer}>
                 {renderCouponList()}
-                {canLoadMore && (
-                  <div className={classes.loadMoreButton}>
-                    <Button
-                      variant='contained'
-                      size='large'
-                      onClick={loadMoreCoupons}
-                    >
-                      {'もっと見る'}
-                    </Button>
-                  </div>
+
+                {coupons?.length > 0 && currentPage > 0 && (
+                  <Pagination
+                    count={countPages}
+                    page={currentPage}
+                    variant={'outlined'}
+                    color={'primary'}
+                    size={'large'}
+                    defaultPage={1}
+                    onChange={changePageCoupons}
+                    className={classes.pagination}
+                  />
                 )}
               </div>
             </>
@@ -222,3 +264,17 @@ const Coupons = () => {
 };
 
 export default Coupons;
+
+Coupons.propTypes = {
+  queryParams: PropTypes.object.isRequired,
+};
+
+export async function getServerSideProps({query}) {
+  const queryParams = {...query, page: Number(query?.page ?? 1), per_page: PER_PAGE};
+
+  return {
+    props: {
+      queryParams,
+    },
+  };
+}
